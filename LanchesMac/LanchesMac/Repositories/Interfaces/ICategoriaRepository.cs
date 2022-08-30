ac50a0d53afa1325e224ee94ec732aa6c2547553
iagolucas2020 <iago0905@hotmail.com>
2022-08-30 08:49:37 -0300

    Criação da controller lanches

--- LanchesMac/LanchesMac/Repositories/Interfaces/ICategoriaRepository.cs
+++ LanchesMac/LanchesMac/Repositories/Interfaces/ICategoriaRepository.cs
@@ -1,6 +1,9 @@
-﻿namespace LanchesMac.Repositories.Interfaces
+﻿using LanchesMac.Models;
+
+namespace LanchesMac.Repositories.Interfaces
 {
     public interface ICategoriaRepository
     {
+        IEnumerable<Categoria> Categorias { get; }  
     }
 }
