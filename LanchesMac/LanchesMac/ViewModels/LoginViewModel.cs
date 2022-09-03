ac111d5455da77d51602a2ff61753a8e4d072249
iagolucas2020 <iago0905@hotmail.com>
2022-09-03 20:21:06 -0300

    autenticação e autorização

---
+++ LanchesMac/LanchesMac/ViewModels/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
+
+namespace LanchesMac.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Informe o nome")]
+        [Display(Name = "Usuário")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Informe a senha")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha")]
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
